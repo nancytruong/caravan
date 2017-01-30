09a535859ca32fbdf241265044a1ab17981a11e0
Nancy Truong <truongh.nancy@gmail.com>
2017-01-30 15:31:52 -0800

    Added a marker and location notification

--- caravan-ios/ViewController.swift
+++ caravan-ios/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import Mapbox
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MGLMapViewDelegate {
+    
+    @IBOutlet var mapView: MGLMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        
+        let point = MGLPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: 35.301355, longitude: -120.660459)
+        point.title = "California Polytechnic San Luis Obispo"
+        point.subtitle = "1 Grand Ave San Luis Obispo CA, U.S.A"
+        mapView.addAnnotation(point)
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        // Always try to show a callout when an annotation is tapped.
+        return true
     }
 
     override func didReceiveMemoryWarning() {
