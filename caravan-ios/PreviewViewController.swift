14dcd188bf85861c3cc36c515ae0373c996c0201
reliu <reliu@calpoly.edu>
2017-06-09 23:17:30 -0700

    Added joining a room but polyline preview doesn't work

--- caravan-ios/PreviewViewController.swift
+++ caravan-ios/PreviewViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  PreviewViewController.swift
 //  caravan-ios
 //
@@ -32,56 +32,122 @@ class PreviewViewController: UIViewController {
     
     var routeDict = Dictionary<String, Any>()
     var route: Route!
+    var currRoute: Route?
+    
+    var roomInput: String = ""
+    
+    var locVal: CLLocationCoordinate2D!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let userId = self.appDelegate.user?.uid
+
+        if (roomInput.isEmpty) {
+            //generate room number
+            var roomIsSet = false
+            var roomArr = [String]()
         
-        //generate room number
-        var roomIsSet = false
-        var roomArr = [String]()
-    
-        ref.child("rooms").observeSingleEvent(of: FIRDataEventType.value, with: { (snapshot) in
-            let enumerator = snapshot.children
-            while let rest = enumerator.nextObject() as? FIRDataSnapshot {
-                roomArr.append(String(describing: rest.key))
-            }
-            while (!roomIsSet) {
-                let num1 = String(arc4random_uniform(9))
-                let num2 = String(arc4random_uniform(9))
-                let num3 = String(arc4random_uniform(9))
-                let num4 = String(arc4random_uniform(9))
-                let tempRoom = String(num1) + String(num2) + String(num3) + String(num4)
-                
-                if (!roomArr.contains(tempRoom)) {
-                    self.room.text = tempRoom
-                    roomIsSet = true
-                
-                    let childUpdates = ["/rooms/\(tempRoom)/finish": [self.route.coordinates?.last?.latitude, self.route.coordinates?.last?.longitude],
-                                        "/rooms/\(tempRoom)/start": [self.route.coordinates?.first?.latitude, self.route.coordinates?.first?.longitude],
-                                        "/rooms/\(tempRoom)/users": userId!] as [String : Any]
-                    self.ref.updateChildValues(childUpdates)
+            ref.child("rooms").observeSingleEvent(of: FIRDataEventType.value, with: { (snapshot) in
+                let enumerator = snapshot.children
+                while let rest = enumerator.nextObject() as? FIRDataSnapshot {
+                    roomArr.append(String(describing: rest.key))
                 }
+                while (!roomIsSet) {
+                    let num1 = String(arc4random_uniform(9))
+                    let num2 = String(arc4random_uniform(9))
+                    let num3 = String(arc4random_uniform(9))
+                    let num4 = String(arc4random_uniform(9))
+                    let tempRoom = String(num1) + String(num2) + String(num3) + String(num4)
+                    
+                    if (!roomArr.contains(tempRoom)) {
+                        self.room.text = tempRoom
+                        roomIsSet = true
+                    
+                        let childUpdates = ["/rooms/\(tempRoom)/finish": [self.route.coordinates?.last?.latitude, self.route.coordinates?.last?.longitude],
+                                            "/rooms/\(tempRoom)/start": [self.route.coordinates?.first?.latitude, self.route.coordinates?.first?.longitude],
+                                            "/rooms/\(tempRoom)/users": userId] as [String : Any]
+                        self.ref.updateChildValues(childUpdates)
+                    }
+                }
+            }) { (error) in
+                print(error.localizedDescription)
             }
-        }) { (error) in
-            print(error.localizedDescription)
-        }
-        
-        
-        preview.setCenter(CLLocationCoordinate2D(latitude: (route.coordinates?.first?.latitude)!,
-                                                 longitude: (route.coordinates?.first?.longitude)!),
-                          zoomLevel: 7, animated: false)
-        
-        if route.coordinateCount > 0 {
-            // Convert the route’s coordinates into a polyline.
-            var routeCoordinates = route.coordinates!
-            let routeLine = MGLPolyline(coordinates: &routeCoordinates, count: route.coordinateCount)
             
-            // Add the polyline to the map and fit the viewport to the polyline.
-            self.preview.addAnnotation(routeLine)
-            self.preview.setVisibleCoordinates(&routeCoordinates, count: route.coordinateCount, edgePadding: .zero, animated: true)
+            preview.setCenter(CLLocationCoordinate2D(latitude: (route.coordinates?.first?.latitude)!,
+                                                     longitude: (route.coordinates?.first?.longitude)!),
+                              zoomLevel: 7, animated: false)
+            
+            if route.coordinateCount > 0 {
+                // Convert the route’s coordinates into a polyline.
+                var routeCoordinates = route.coordinates!
+                let routeLine = MGLPolyline(coordinates: &routeCoordinates, count: route.coordinateCount)
+                
+                // Add the polyline to the map and fit the viewport to the polyline.
+                self.preview.addAnnotation(routeLine)
+                self.preview.setVisibleCoordinates(&routeCoordinates, count: route.coordinateCount, edgePadding: .zero, animated: true)
+            }
         }
+        else {
+            room.text = roomInput
+            
+            
+            ref.child("rooms").child(roomInput).child("finish").observeSingleEvent(of: .value, with: { (snapshot) in
+
+                var temp = [CLLocationDegrees]()
+                if let coord = snapshot.value as? NSArray{
+                    for i in 0..<coord.count {
+                        temp.append(coord[i] as! CLLocationDegrees)
+                    }
+                }
+                
+                let dest = CLLocationCoordinate2D(latitude: temp[0], longitude: temp[1])
+                
+                let waypoints = [
+                    Waypoint(
+                        coordinate: self.locVal,
+                        name: "Current Location"
+                    ),
+                    Waypoint(
+                        coordinate: dest,
+                        name: "Destination" //should we change to real destination name?
+                    ),
+                    ]
+                let options = RouteOptions(waypoints: waypoints, profileIdentifier: MBDirectionsProfileIdentifier.automobile)
+                options.includesSteps = true
+                options.includesAlternativeRoutes = true;
+                _ = self.directions.calculate(options) { (waypoints, routes, error) in
+                    guard error == nil else {
+                        print("Error calculating directions: \(error!)")
+                        return
+                    }
+                    if let tempRoute = routes?.first {
+                        let currRoute = tempRoute
+                    }
+                }
+                
+                
+                self.preview.setCenter(CLLocationCoordinate2D(latitude: self.locVal.latitude, longitude: self.locVal.longitude), zoomLevel: 7, animated: false)
+                //IDK HOW TO FIX THISSSSS
+                /*
+                if ((self.currRoute?.coordinateCount)! > 0) {
+                    // Convert the route’s coordinates into a polyline.
+                    var routeCoordinates = self.currRoute?.coordinates!
+                    let routeLine = MGLPolyline(coordinates: &routeCoordinates, count: self.currRoute?.coordinateCount)
+
+                    // Add the polyline to the map and fit the viewport to the polyline.
+                    self.preview.addAnnotation(routeLine)
+                    self.preview.setVisibleCoordinates(&routeCoordinates, count: self.currRoute?.coordinateCount, edgePadding: .zero, animated: true)
+                }
+                */
+                
+            }) { (error) in
+                print(error.localizedDescription)
+            }
+            
+        }
+        
+        
         
         
         // DO THIS AFTER THIS CONTROLLER IS DONE:
